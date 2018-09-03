48edaa1c164cd3bcbe0a737b3ae3e6b42196314c
dmaish <mainadaniel81@gmail.com>
2018-09-03 10:18:58 +0300

    add modal to confirm user actually wants to log out

--- src/components/profilePage.js
+++ src/components/profilePage.js
@@ -4,6 +4,9 @@ import {Link} from 'react-router-dom'
 import Library from './libraryComponent'
 import UserHistory from './userHistory'
 import ReturnBook from './returnBook'
+import swal from 'sweetalert';
+import history from '../utils/history'
+
 
 class ProfilePage extends Component{
     constructor(props){
@@ -11,7 +14,52 @@ class ProfilePage extends Component{
         this.state = {
 
         }
+        this.logoutAlert = this.logoutAlert.bind(this);
+        this.actualLogout = this.actualLogout.bind(this);
     }
+
+    //function to handle user logging out
+    logoutAlert(){
+        swal({
+            title: "Are you sure...",
+            text: "you want to log out?",
+            icon: "warning",
+            buttons: {
+                    cancel: "Cancel",
+                    okay: true,
+            }
+          })
+          .then((value) => {
+            switch (value) {
+                case "cancel":
+                  swal("cancelled");
+                  break;
+                case "okay":
+                    this.actualLogout()
+                    swal("logout", "was a success!", "success");
+                    break;
+                default:
+                  break;
+              }
+          });
+    }
+
+    // actual logout call to api
+    actualLogout(){
+        const jwt_token = localStorage.getItem('access_token')
+        fetch("http://127.0.0.1:5000/api/auth/logout",{
+            method: "POST",
+            headers: {"Content-Type": "application/json",
+                        'Authorization': `Bearer ${jwt_token}`},
+            body: JSON.stringify(this.state)
+        }).then(response => response.json())
+        .then(response => {
+            swal(response.message)
+            history.push('/')
+        })
+    }
+
+
     render(){
         return(
         <div className="container">
@@ -19,9 +67,11 @@ class ProfilePage extends Component{
                     {/* column containing the profile details */}
                     <div className="col-md-3 col-sm-6">
                         <img className="avatar" src={avatar} alt=''/>
-                        <h6><Link to='/resetPassword' className="btn btn-primary btn-sm">reset password</Link></h6>
+                        <h6><Link to='/resetPassword' className="btn btn-secondary"> reset password </Link></h6>
+                        <button type="button" class="btn btn-secondary" onClick={this.logoutAlert}> logout </button>
                     </div>
                 {/* column containing books to be returned and the table showing history */}
+                {/* THIS IS A BOOTSTRAP TABLIST (NAVBAR) */}
                 <div className="col-md-9 col-sm-6">
                 <ul className="nav nav-tabs" id="myTab" role="tablist">
                     <li className="nav-item">
