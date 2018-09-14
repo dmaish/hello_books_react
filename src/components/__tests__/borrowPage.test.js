baf8c0a3abeec8d3d2aa848199d708cfd453be47
dmaish <mainadaniel81@gmail.com>
2018-09-14 10:10:29 +0300

    tests elevated

--- src/components/__tests__/borrowPage.test.js
+++ src/components/__tests__/borrowPage.test.js
@@ -5,7 +5,7 @@ import BorrowPage from "../borrowPage";
 describe("borrow page renders", () => {
     const component = shallow(<BorrowPage/>)
     it("navbar renders", () => {  
-        expect(component.find("nav.navbar").exists()).toBe(true)
+        expect(component.find(".navbar").exists()).toBe(true)
     })
     it("row renders", () => {
         expect(component.find("div.row").exists()).toBe(true)
--- src/components/__tests__/signin.test.js
+++ src/components/__tests__/signin.test.js
@@ -2,13 +2,6 @@ import React from "react";
 import { shallow } from "enzyme";
 import SignIn from "../signin";
 
-describe("My signIn component", () => {
-	it("should render correctly in \"debug\" mode", () => {
-		const wrapper = shallow(<SignIn/>)
-        expect(wrapper).toMatchSnapshot()
-    })
-})
-
 //make our assertion that form in component is being rendered
 // make sure the inputs on the signin form also render correctly
 describe("Sigin component renders well", () => {
